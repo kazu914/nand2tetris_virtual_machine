5d719659d85ea6aebdd0a088df0641dd63ced6ee
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-11-07 18:48:08 +0900

    added add function

--- src/code_writer.rs
+++ src/code_writer.rs
@@ -28,6 +28,36 @@ impl Segment {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum ArithmeticCommand {
+    ADD,
+    SUB,
+    NEG,
+    EQ,
+    GT,
+    LT,
+    AND,
+    OR,
+    NOT,
+}
+
+impl ArithmeticCommand {
+    pub fn from_str(s: &str) -> Option<ArithmeticCommand> {
+        match s {
+            "add" => Some(ArithmeticCommand::ADD),
+            "sub" => Some(ArithmeticCommand::SUB),
+            "neg" => Some(ArithmeticCommand::NEG),
+            "eq" => Some(ArithmeticCommand::EQ),
+            "gt" => Some(ArithmeticCommand::GT),
+            "lt" => Some(ArithmeticCommand::LT),
+            "and" => Some(ArithmeticCommand::AND),
+            "or" => Some(ArithmeticCommand::OR),
+            "not" => Some(ArithmeticCommand::NOT),
+            _ => panic!("Invalid ArithmeticCommand"),
+        }
+    }
+}
+
 pub struct CodeWriter {
     file_name: String,
     stack_point: usize,
@@ -54,12 +84,32 @@ impl CodeWriter {
         }
     }
 
+    pub fn run_arichmetic_command(&mut self, arithmetic_command: &str) {
+        match ArithmeticCommand::from_str(arithmetic_command) {
+            Some(ArithmeticCommand::ADD) => {
+                let mut commands = CodeWriter::add(self.stack_point);
+                self.stack_point -= 1;
+                self.generated_code.append(&mut commands)
+            }
+            _ => (),
+        }
+    }
+
     fn push_constant(stack_point: usize, constant: &str) -> Vec<String> {
         vec![
             format!("@{}", constant),
             "D=A".to_string(),
             format!("@{}", stack_point),
             "M=D".to_string(),
+        ]
+    }
+
+    fn add(stack_point: usize) -> Vec<String> {
+        vec![
+            format!("@{}", stack_point - 1),
+            "D=M".to_string(),
+            format!("@{}", stack_point - 2),
+            "M=M+D".to_string(),
         ]
     }
 }
@@ -76,6 +126,18 @@ mod test {
             "M=D".to_string(),
         ];
         let result = CodeWriter::push_constant(256, "7");
+        assert_eq!(result, expected_result)
+    }
+
+    #[test]
+    fn add() {
+        let expected_result = vec![
+            "@257".to_string(),
+            "D=M".to_string(),
+            "@256".to_string(),
+            "M=M+D".to_string(),
+        ];
+        let result = CodeWriter::add(258);
         assert_eq!(result, expected_result)
     }
 }
