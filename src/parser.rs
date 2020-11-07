c5de97b3153ca3ec42f7b6934579bb219d2fe712
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-11-07 14:12:45 +0900

    added unnecessary part remove function in constructor

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum CommandType {
     ARITHMETIC,
     PUSH,
@@ -21,11 +22,11 @@ pub struct Parser {
 
 impl Parser {
     pub fn new(commands: Vec<String>) -> Parser {
-        let has_more_commands = commands.len() > 0;
+        let actual_commands = Parser::remove_unnecessary_parts(commands);
         Parser {
-            commands,
+            has_more_commands: actual_commands.len() > 0,
+            commands: actual_commands,
             index: 0,
-            has_more_commands,
             command_type: None,
             arg1: None,
             arg2: None,
@@ -57,13 +58,13 @@ impl Parser {
 mod test {
     use super::*;
     #[test]
-    fn test_remove_comments() {
+    fn parer_remove_comments() {
         let command = "   push constant 7  // this is comment".to_string();
         assert_eq!(Parser::remove_comments(command), "push constant 7");
     }
 
     #[test]
-    fn test_remove_unnecessary_parts() {
+    fn parer_remove_unnecessary_parts() {
         let original_commands = vec![
             "//this is comment line".to_string(),
             "push constant 7 // here also comment".to_string(),
@@ -75,6 +76,23 @@ mod test {
             Parser::remove_unnecessary_parts(original_commands),
             new_commands
         );
+    }
+    #[test]
+    fn parser_new() {
+        let original_commands = vec![
+            "//this is comment line".to_string(),
+            "push constant 7 // here also comment".to_string(),
+            "   add    //whitespace should be trimmed".to_string(),
+        ];
+
+        let new_commands = vec!["push constant 7".to_string(), "add".to_string()];
+        let parer = Parser::new(original_commands);
+        assert_eq!(parer.commands, new_commands);
+        assert_eq!(parer.has_more_commands, true);
+        assert_eq!(parer.index, 0);
+        assert_eq!(parer.command_type, None);
+        assert_eq!(parer.arg1, None);
+        assert_eq!(parer.arg2, None);
     }
 }
 
