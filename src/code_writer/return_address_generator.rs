d6bcbed4b5f268c6f5c14f87e0ad0e472ee5b69c
Kazuaki Nomura <nkz0914ssk@gmail.com>
2021-02-21 18:21:17 +0900

    :sparkles: (code_writer) add return_address_generator

---
+++ src/code_writer/return_address_generator.rs
@@ -0,0 +1,36 @@
+pub struct ReturnAddressGenerator {
+    counter: usize,
+}
+
+impl ReturnAddressGenerator {
+    pub fn new() -> ReturnAddressGenerator {
+        ReturnAddressGenerator { counter: 0 }
+    }
+
+    pub fn generate_new_return_address(&mut self) -> String {
+        self.counter += 1;
+        format!("return_address.{}", self.counter)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn generate_new_return_address() {
+        let mut return_address_generator = ReturnAddressGenerator::new();
+        let return_address: String = return_address_generator.generate_new_return_address();
+        assert_eq!("return_address.1", return_address)
+    }
+
+    #[test]
+    fn increment_counter() {
+        let mut return_address_generator = ReturnAddressGenerator::new();
+        let _ = return_address_generator.generate_new_return_address();
+        assert_eq!(1, return_address_generator.counter);
+
+        let _ = return_address_generator.generate_new_return_address();
+        assert_eq!(2, return_address_generator.counter)
+    }
+}
